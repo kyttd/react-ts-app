a7348dba8065e178a82b78b744962f042dea6e03
kyttd <kuyt1992@gmail.com>
2022-02-28 00:16:03 +0900

    feat: Textコンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import styled from "styled-components";
+import { Text } from "./libs/Text";
+
 export const App = () => {
-  return (
-    <h1>React App</h1>
-  )
-}
+  return <StyledText text="Hello, World!" />;
+};
+
+const StyledText = styled(Text)`
+  font-size: 2rem;
+`;
